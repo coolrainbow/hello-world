f663e54f687a4686380fb41d3fe0869b309abdb9
Jun Zhang <zhangjunqcc@gmail.com>
2017-07-25 13:31:11 -0500

    Add math module

---
+++ hello_world/__init__.py
@@ -0,0 +1,6 @@
+"""
+This is the base file of the hello-world!
+"""
+
+from . import math
+from .math import mult
---
+++ hello_world/math.py
@@ -0,0 +1,16 @@
+"""
+A small set of functions for doing math operations.
+"""
+
+# Add two values
+def add(a, b):
+    """
+    Add two values.
+    """
+    return 2+5
+
+def mult(a, b):
+    """
+    Multiply two values.
+    """
+    return 2*5
